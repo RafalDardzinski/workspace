368d66358b79603abd7758b675e467693b4d534e
Rafał Dardziński <rafal.dardzinski@gmail.com>
2026-04-08 00:39:27 +0200

    mend

--- packages/oh-my-zsh.sh
+++ packages/oh-my-zsh.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env zsh
 # Installs oh-my-zsh shell extension.
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && printf '%s\n' "source .ohmyzshrc" >> "${ZDOTDIR}/.zshrc"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && printf '%s\n' "source ${ZDOTDIR}/.ohmyzshrc" >> "${ZDOTDIR}/.zshrc"
